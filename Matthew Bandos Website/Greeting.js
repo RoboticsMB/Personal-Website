4a68283c3162419549e8e7479206d279de55a523
RoboticsMB <83793330+RoboticsMB@users.noreply.github.com>
2023-06-24 17:00:41 -0400

    Changed the colors slightly as well as adjusted the js file to account for scenarios where js loads before the html file

--- Matthew Bandos Website/Greeting.js
+++ Matthew Bandos Website/Greeting.js
@@ -1,35 +1,36 @@
-// Get the user's current date and time
-var currentTime = new Date();
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the user's current date and time
+    var currentTime = new Date();
 
-// Get the user's timezone offset in minutes
-var timezoneOffset = currentTime.getTimezoneOffset();
+    // Get the user's timezone offset in minutes
+    var timezoneOffset = currentTime.getTimezoneOffset();
 
-// Convert the timezone offset to hours
-var timezoneOffsetHours = Math.abs(timezoneOffset / 60);
+    // Convert the timezone offset to hours
+    var timezoneOffsetHours = Math.abs(timezoneOffset / 60);
 
-// Get the user's current hour in their timezone
-var currentHour = currentTime.getHours() + timezoneOffsetHours;
+    // Get the user's current hour in their timezone
+    var currentHour = currentTime.getHours() + timezoneOffsetHours;
 
-// Define the greetings based on the time of day
-var morningGreeting = "Good morning! I'm Matthew!";
-var afternoonGreeting = "Good afternoon! I'm Matthew!";
-var eveningGreeting = "Good evening! I'm Matthew!";
-var defaultGreeting = "Hello! I'm Matthew!";
+    // Define the greetings based on the time of day
+    var morningGreeting = "Good morning! I'm Matthew!";
+    var afternoonGreeting = "Good afternoon! I'm Matthew!";
+    var eveningGreeting = "Good evening! I'm Matthew!";
+    var defaultGreeting = "Hello! I'm Matthew!";
 
-// Determine the appropriate greeting based on the current hour
-var greeting;
-if (currentHour >= 0 && currentHour < 12) {
-    greeting = morningGreeting;
-} else if (currentHour >= 12 && currentHour < 17) {
-    greeting = afternoonGreeting;
-} else {
-    greeting = eveningGreeting;
-}
+    // Determine the appropriate greeting based on the current hour
+    var greeting;
+    if (currentHour >= 0 && currentHour < 12) {
+        greeting = morningGreeting;
+    } else if (currentHour >= 12 && currentHour < 17) {
+        greeting = afternoonGreeting;
+    } else {
+        greeting = eveningGreeting;
+    }
 
+    if (typeof greeting === "undefined") {
+        greeting = defaultGreeting;
+    }
 
-if (typeof greeting === "undefined") {
-    greeting = defaultGreeting;
-}
-
-// Display the greeting on the website
-document.getElementById("greeting").textContent = greeting;
+    // Display the greeting on the website
+    document.getElementById("greeting").textContent = greeting;
+});
